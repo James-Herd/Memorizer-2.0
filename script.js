4826fbfe7129a18c55a78cbbab4d004cf839d305
James Herd <james.o.herd@gmail.com>
2024-11-17 17:56:41 +1000

    updated colour pallette and timing animations on header

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", () => {
+  setCopyrightDate();
+});
+
+const setCopyrightDate = () => {
   const currentYear = new Date().getFullYear();
   const copyrightDateElement = document.getElementById("copyright-date");
   copyrightDateElement.innerText = currentYear;
-});
+};
